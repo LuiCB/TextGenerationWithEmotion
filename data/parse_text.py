9249000e33f4109b93aba2cbc1ff6959142839ec
dorasir <dorasir@gmail.com>
2018-04-12 21:10:34 -0400

    fixed indentation

--- data/parse_text.py
+++ data/parse_text.py
@@ -4,7 +4,6 @@ import io
 import string
 import json
 
-
 data_dir = './aclimdb/'
 output_dir = './aclimdb/'
 files = os.listdir(data_dir + 'train/pos')
@@ -13,30 +12,28 @@ dictionary = set()
 review_f = open('pos_review.txt', 'w', encoding='utf-8')
 rate_f = open('pos_rate.txt', 'w', encoding='utf-8')
 with open(data_dir + 'imdb.vocab', 'r', encoding='utf-8') as d:
-	for line in d:
-		dictionary.add(line.strip())
+    for line in d:
+        dictionary.add(line.strip())
 for file in files:
-	count += 1
-	args = re.search('(.*)_(.*)\.txt', file)
-	id = int(args.group(1))
-	rate = int(args.group(2))
+    count += 1
+    args = re.search('(.*)_(.*)\.txt', file)
+    id = int(args.group(1))
+    rate = int(args.group(2))
 
-	f = open(data_dir + 'train/pos/' + file, encoding='utf8')
-	iter_f = iter(f)
-	s = ''
-	for line in iter_f:
-		s = s + line
-	words = s.split()
-	ns = ''
-	for word in words:
-		word.replace('<br />', ' ')
-		word = word.strip("?:!.,;])\"\'").lower()
-		if word in dictionary:
-			ns = ns + word + ' '
-	ns = ns.strip()
-	review_f.write(ns + '\n')
-	rate_f.write(str(id) + ' ' + str(rate) + '\n')
+    f = open(data_dir + 'train/pos/' + file, encoding='utf8')
+    iter_f = iter(f)
+    s = ''
+    for line in iter_f:
+        s = s + line
+    words = s.split()
+    ns = ''
+    for word in words:
+        word.replace('<br />', ' ')
+        word = word.strip("?:!.,;])\"\'").lower()
+        if word in dictionary:
+            ns = ns + word + ' '
+    ns = ns.strip()
+    review_f.write(ns + '\n')
+    rate_f.write(str(id) + ' ' + str(rate) + '\n')
 review_f.close()
 rate_f.close()
-
-
